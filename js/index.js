79586142d23583dba71c7be2a97775bd93bd8b54
ViktoriaTorianik <122830538+ViktoriaTorianik@users.noreply.github.com>
2023-04-08 11:17:22 +0300

    ECMAScript

---
+++ js/index.js
@@ -0,0 +1,14 @@
+//  Використовуючи функцію if...else,
+//   *? напишіть код, який запитуватиме:
+//   *? "Яка офіційна назва JavaScript?"
+//   *? Якщо користувач вводить "ECMAScript",
+//   *? то показати через alert: "Вірно!"
+//   *? інакше відобразити:"Не знаєте? ECMAScript!"
+const messege = prompt("Яка офіційна назва JavaScript?").trim().toLowerCase();
+let answer = "ECMAScript".toLowerCase()
+console.log(messege)
+if(messege === answer){
+    alert("Вірно!")
+} else {
+    alert("Не знаєте? ECMAScript!")
+}
